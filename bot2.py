bfcc339f9ca1fd6fc02965f58c71ba68c5a5f3e3
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-03-20 00:29:42 -0300

    Atualizar o bot2.py

--- bot2.py
+++ bot2.py
@@ -3,10 +3,11 @@ import requests
 TOKEN = "8656227637:AAHSMvZd31r_aG70NZG5eyFoYmivjCgx4UA"
 CHAT_ID = "@achadinhosshopeebr4"
 
-requests.post(
-    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-    data={
-        "chat_id": CHAT_ID,
-        "text": "🔥 BOT NOVO FUNCIONANDO"
-    }
-)
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+res = requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": "🔥 BOT FUNCIONANDO COM TOKEN NOVO"
+})
+
+print(res.text)
