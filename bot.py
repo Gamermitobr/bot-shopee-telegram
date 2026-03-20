d8450acfa9d30044397fb3e18d6cc34861c9307d
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-03-19 23:27:42 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,13 @@
 import requests
-from bs4 import BeautifulSoup
 
 TOKEN = "8656227637:AAFAceWhP-4n9XFrj1YFPxfHWLTxfBRu0Tk"
-CHAT_ID = "3562877087"
+CHAT_ID = "@achadinhosshopeebr4"
 
-def pegar_ofertas():
-    url = "https://www.pelando.com.br/grupo/shopee"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "html.parser")
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-    ofertas = []
+res = requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": "🚀 TESTE: BOT FUNCIONANDO!"
+})
 
-    for item in soup.select(".thread-link")[:5]:
-        titulo = item.get_text().strip()
-        link = "https://www.pelando.com.br" + item.get("href")
-
-        ofertas.append({
-            "titulo": titulo,
-            "link": link
-        })
-
-    return ofertas
-
-def gerar_mensagem(oferta):
-    return f"""🔥 PROMOÇÃO REAL 🔥
-
-🛒 {oferta['titulo']}
-
-👉 {oferta['link']}
-
-⏰ Corre antes que acabe!
-"""
-
-def enviar(msg):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
-
-if __name__ == "__main__":
-    ofertas = pegar_ofertas()
-
-    for oferta in ofertas:
-        msg = gerar_mensagem(oferta)
-        enviar(msg)
+print(res.text)
