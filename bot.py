06b1af980386f0ce34b9c86aa2b71b56264513b3
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-03-20 13:09:56 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,22 @@
 import requests
 
+print("INICIOU O BOT")
+
 TOKEN = "8656227637:AAG1wxbRplphnglvWrLmpnbwyrEslqlsvKg"
 CHAT_ID = "@achadinhosshopeebr4"
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-response = requests.post(url, data={
-    "chat_id": CHAT_ID,
-    "text": "Mensagem enviada pelo GitHub 🚀"
-})
+try:
+    response = requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text": "Mensagem enviada pelo GitHub 🚀"
+    })
 
-print(response.text)
-print("BOT INICIADO")
+    print("STATUS:", response.status_code)
+    print("RESPOSTA:", response.text)
+
+except Exception as e:
+    print("ERRO:", e)
+
+print("FIM DO BOT")
