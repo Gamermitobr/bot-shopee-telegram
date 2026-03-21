c3ace1fa920a92cb8dd377c0f46f905a9aae946d
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-03-21 11:49:58 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,6 @@
-import requests
-import time
-
-TOKEN = "8656227637:AAG1wxbRplphnglvWrLmpnbwyrEslqlsvKg"
-CHAT_ID = "@achadinhosshopeebr4"
-URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-while True:
-    requests.get(URL, params={"chat_id": CHAT_ID, "text": "🔥 Mensagem automática 🚀"})
-    time.sleep(60)  # envia a cada 60 segundos
+  # bot.py
+  import os, requests
+  TOKEN = os.getenv("8656227637:AAG1wxbRplphnglvWrLmpnbwyrEslqlsvKg")
+  CHAT_ID = os.getenv("@achadinhosshopeebr4")
+  URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+  requests.get(URL, params={"chat_id": CHAT_ID, "text": "🔥 Mensagem automática 🚀"})
