17ef33e1459408ab0e4e2520c81550314dc7395c
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-03-21 19:40:43 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import os
 import requests
 
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TOKEN = os.getenv("8656227637:AAH4qVemew_e6O55Y2yc48x-Ko9eh8Ad-BI")
+CHAT_ID = os.getenv("https://t.me/achadinhosshopeebr4")
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 payload = {
