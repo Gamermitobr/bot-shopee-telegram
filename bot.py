18dde1f1e1f4503f24a35158e804acbaf37e6cd4
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-03-21 11:58:03 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -4,3 +4,4 @@
   CHAT_ID = os.getenv("@achadinhosshopeebr4")
   URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
   requests.get(URL, params={"chat_id": CHAT_ID, "text": "🔥 Mensagem automática 🚀"})
+   print("Resposta Telegram:", resp.json())
