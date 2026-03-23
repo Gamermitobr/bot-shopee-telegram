e9e9222e84b9d0da8e76fc47fe2ba039b665ab5d
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-03-23 16:24:39 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import os, requests
 
-TOKEN = os.environ["TELEGRAM_TOKEN"]  # vem do secret
-CHAT_ID = os.environ["TELEGRAM_CHAT"] # ex: -1003562877087
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT"]
 
-msg = "teste de ofertas"
+print("token len:", len(TOKEN))  # só pra depurar
+
+msg = "ping"
 url = f"https://api.telegram.org/bot{8656227637:AAH4qVemew_e6O55Y2yc48x-Ko9eh8Ad-BI}/sendMessage"
 r = requests.post(url, json={"chat_id": CHAT_ID, "text": msg})
 print(r.status_code, r.json())
