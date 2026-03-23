e2a52d53caf3a636dfd961e2aa577e633bacab5f
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-03-22 21:39:17 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import os
 import requests
 
-TOKEN = os.getenv("8656227637:AAH4qVemew_e6O55Y2yc48x-Ko9eh8Ad-BI")  # coloca o token nos Secrets também
-CHAT_ID = -1003562877087  # <-- teu canal
+TOKEN = "8656227637:AAH4qVemew_e6O55Y2yc48x-Ko9eh8Ad-BI"  # teu token
+CHAT_ID = -1003562877087  # <-- canal
 
 def main():
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
