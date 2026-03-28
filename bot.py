011ae3945e1e23f848d7d6d61fad9cc3fbf9e0b4
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-03-27 23:02:20 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -3,25 +3,12 @@ import requests
 import os
 
 TOKEN = os.getenv("8276947745:AAFwt9r7CnjGkzx_aNiYmiwf6B0QbDZ_9iA")
-CHAT_ID = os.getenv("https://t.me/fftechsegredo")
-RSS_URL = os.getenv("https://www.youtube.com/feeds/videos.xml?channel_id=UCB0m5QfGQ0lF3ZVn9F0t9rA")
-
-ARQUIVO = "ultimo.txt"
+CHAT_ID = "@fftechsegredo"
+RSS_URL = "https://www.youtube.com/feeds/videos.xml?channel_id=UCB0m5QfGQ0lF3ZVn9F0t9rA"
 
 def enviar(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
-
-def ler_ultimo():
-    try:
-        with open(ARQUIVO, "r") as f:
-            return f.read().strip()
-    except:
-        return ""
-
-def salvar_ultimo(link):
-    with open(ARQUIVO, "w") as f:
-        f.write(link)
 
 feed = feedparser.parse(RSS_URL)
 
@@ -30,17 +17,5 @@ if feed.entries:
     titulo = video.title
     link = video.link
 
-    ultimo = ler_ultimo()
-
-    if link != ultimo:
-        mensagem = f"""🔥 NOVO VÍDEO!
-
-{titulo}
-
-📺 Assista agora:
-{link}
-
-🚀 Entra no canal pra mais dicas!"""
-        
-        enviar(mensagem)
-        salvar_ultimo(link)
+    mensagem = f"🔥 NOVO VÍDEO!\n\n{titulo}\n\n📺 Assista: {link}"
+    enviar(mensagem)
