97d7f911565631db0a9c85acf9bff0f070cbfa6a
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-07-03 20:31:17 -0300

    Criar bot.js

---
+++ bot.js
@@ -0,0 +1,51 @@
+const fetch = require("node-fetch");
+
+const TOKEN = process.env.TOKEN;
+
+// canais
+const canais = [
+"@robinairdraktutoriais",
+"@canal2"
+];
+
+// produtos
+const produtos = [
+"https://s.shopee.com.br/6ffHxillyX"
+];
+
+function montarMensagem(link) {
+return `🔥 OFERTA RELÂMPAGO 🔥
+
+🛒 Produto na Shopee
+
+🔗 ${link}
+
+⚠️ Corre antes que acabe!`;
+}
+
+async function enviar(mensagem) {
+for (let canal of canais) {
+await fetch("https://api.telegram.org/bot${TOKEN}/sendMessage", {
+method: "POST",
+headers: {
+"Content-Type": "application/json"
+},
+body: JSON.stringify({
+chat_id: canal,
+text: mensagem
+})
+});
+
+console.log("Enviado para", canal);
+
+}
+}
+
+async function rodar() {
+for (let link of produtos) {
+const msg = montarMensagem(link);
+await enviar(msg);
+}
+}
+
+rodar();
