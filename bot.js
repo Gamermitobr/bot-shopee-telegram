e9d905b35f9336ce82aa2083a32694bc5ccc9476
MODS <67655889+Gamermitobr@users.noreply.github.com>
2026-07-03 21:02:12 -0300

    Atualizar o bot.js

--- bot.js
+++ bot.js
@@ -25,18 +25,27 @@ function montarMensagem(link) {
 
 async function enviar(mensagem) {
   for (let canal of canais) {
-    await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        chat_id: canal,
-        text: mensagem
-      })
-    });
+    try {
+      const res = await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          chat_id: canal,
+          text: mensagem
+        })
+      });
 
-    console.log("Enviado para", canal);
+      const data = await res.json();
+
+      console.log("📤 Enviado para:", canal);
+      console.log("📩 Resposta Telegram:", data);
+
+    } catch (erro) {
+      console.log("❌ Erro ao enviar para:", canal);
+      console.log(erro);
+    }
   }
 }
 
